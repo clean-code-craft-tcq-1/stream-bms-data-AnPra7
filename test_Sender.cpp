cadf0f5f09cf50140d061feb9919b2f77a04698a
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-02 17:54:22 +0530

    Create test_Sender.cpp

---
+++ test_Sender.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN 
+
+#include "catch.hpp"
+#include "Sender/Sender.h"
+
+
+TEST_CASE("Check if trasmissions are successfull") 
+{
+ SendInput inputdataofBMS= file;
+ REQUIRE(SendInput (inputdataofBMS) == OK);
+}
+
+TEST_CASE("Testcase to check if the console print is happening periodically") 
+{
+ 
+SendInput inputdataofBMS = file;
+BMS_outputtype outputvaluefetch = printtoconsole;
+REQUIRE(SendInput (inputdataofBMS) == OK);
+REQUIRE(SendOutput  (outputdataofBMS) == OK);
+}
