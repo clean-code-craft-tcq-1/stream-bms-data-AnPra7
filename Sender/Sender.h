db1068e6398d135e7f588d4d2e4b8f92748e4689
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-02 11:37:18 +0530

    Create Sender.h

---
+++ Sender/Sender.h
@@ -0,0 +1,11 @@
+/*!
+***     \file        BMS_DataSender.h
+***
+***     \author      Abinesh KanjiKovil Thatchinamoorthy
+***
+\par    File_description
+***                  Header for the BMS data streaming source file
+*\n*/
+/*********************************************************************************/
+
+// Enums and functions
