99bc1a08e569ae882b73b9b8da51841a9906d2a4
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-02 13:12:16 +0530

    Update Sender.h

--- Sender/Sender.h
+++ Sender/Sender.h
@@ -1,11 +1,48 @@
 /*!
-***     \file        BMS_DataSender.h
+***     \file        Sender.h
 ***
-***     \author      Abinesh KanjiKovil Thatchinamoorthy
+***     \author      pratik
 ***
-\par    File_description
-***                  Header for the BMS data streaming source file
+\par    File_description Header for the  data streaming source file
 *\n*/
+                  
 /*********************************************************************************/
 
+//Definitions and macros
+
+#define DATA_QUANT 50
+extern float Temperature_values[DATA_QUANT];
+extern float SOC_Values[DATA_QUANT];
+
 // Enums and functions
+
+typedef enum
+{
+  NOT_OK,
+  OK
+}Status;
+
+typedef enum
+{
+  toConsole,
+  toController
+}SendOutput;
+
+typedef enum
+{
+  file,
+  server
+}SendInput;
+
+
+/*------------------------------Functions---------------------------------------------*/
+
+/*------------------------------Declarations------------------------------------------*/
+
+Status InputtotheSystem(SendInput inputdataofBMS);
+Status OutputoftheSyatem(SendInput inputdataofBMS);
+Status Scandata(float Temperature_values[],float SOC_Values[]);
+Status ToConsole(float Temperature_values[],float SOC_Values[]);
+
+
+
