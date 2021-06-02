3fea99ef6a8b7b352e437efe6154ef03ec912113
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-02 19:06:10 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -7,7 +7,7 @@
 TEST_CASE("Check if trasmissions are successfull") 
 {
  SendInput inputdataofBMS= file;
- REQUIRE(InputtotheSystem(inputdataofBMS) == NOT_OK);
+ REQUIRE(InputtotheSystem(inputdataofBMS) == OK);
 }
 
 TEST_CASE("Testcase to check if the console print is happening periodically") 
@@ -15,6 +15,6 @@ TEST_CASE("Testcase to check if the console print is happening periodically")
  
 SendInput inputdataofBMS = file;
 SendOutput outputdataofBMS = toConsole;
-REQUIRE(InputtotheSystem(inputdataofBMS) == NOT_OK);
-REQUIRE(OutputoftheSyatem(outputdataofBMS) == NOT_OK);
+REQUIRE(InputtotheSystem(inputdataofBMS) == OK);
+REQUIRE(OutputoftheSyatem(outputdataofBMS) == OK);
 }
